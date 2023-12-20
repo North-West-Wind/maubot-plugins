fbd48b0476e3399a722eb77e707a73a2356b1695
North-West-Wind <wsyn148@gmail.com>
2023-12-21 00:50:30 +0800

    independence!

--- maubot-color/color/bot.py
+++ maubot-color/color/bot.py
@@ -1,6 +1,14 @@
+import io
 from maubot import Plugin, MessageEvent
 from maubot.handlers import command
+from mautrix.types import ImageInfo, ThumbnailInfo
+from PIL import Image
 import random
+from typing import TypedDict
+
+class UploadedImage(TypedDict):
+	url: str
+	info: ImageInfo
 
 class ColorBot(Plugin):
 	@command.new(require_subcommand=True)
@@ -14,9 +22,40 @@ class ColorBot(Plugin):
 		b = random.randint(0, 255)
 		rgb = [r, g, b]
 		hexColor = ''.join(list(map(lambda x: (hex(x).split('x')[-1]).zfill(2).upper(), rgb)))
+		#img: UploadedImage = await self._upload_image(rgb)
+		#url = img["url"]
+		#info = img["info"]
 		await evt.reply("Here's a random color: #" + hexColor + "\nhttps://www.northwestw.in/color/" + hexColor, markdown=False)
+		#await self.client.send_image(evt.room_id, url, info)
 
 	@color.subcommand("show", help="Preview the given color.", aliases=("s"))
 	@command.argument("hexColor", required=True, matches="[\da-fA-F]{6}", pass_raw=True)
 	async def show(self, evt: MessageEvent, hexColor: str) -> None:
+		#parsedHex = int(hexColor, 16)
+		#b = parsedHex % 0x100
+		#g = (parsedHex // 0x100) % 0x100
+		#r = parsedHex // 0x10000
+		#rgb = [r, g, b]
+		#img: UploadedImage = await self._upload_image(rgb)
+		#url = img["url"]
+		#info = img["info"]
 		await evt.reply("Here's your color: #" + hexColor + "\nhttps://www.northwestw.in/color/" + hexColor, markdown=False)
+		#await self.client.send_image(evt.room_id, url)
+
+	"""
+	async def _upload_image(self, rgb: list[int]) -> UploadedImage:
+		hexColor = ''.join(list(map(lambda x: (hex(x).split('x')[-1]).zfill(2).upper(), rgb)))
+		info = ImageInfo()
+		# generate the image
+		img = Image.new("RGB", (128, 32), tuple(rgb))
+		imgByteArr = io.BytesIO()
+		img.save(imgByteArr, format="PNG")
+		imgByteArr = imgByteArr.getvalue()
+		# write image info
+		info.mimetype = "image/png"
+		info.size = len(imgByteArr)
+		info.width, info.height = img.size
+		# upload color image
+		url = await self.client.upload_media(imgByteArr, info.mimetype, hexColor + ".png")
+		return { "url": url, "info": info }
+	"""
